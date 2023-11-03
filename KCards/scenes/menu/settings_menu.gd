fdd2b2832bc73deec74cbf6f53d576b4a44c2e9d
mu2so4 <b4.orangutang@gmail.com>
2023-11-03 20:38:08 +0700

    Add enabling and disabling fullscreen

--- KCards/scenes/menu/settings_menu.gd
+++ KCards/scenes/menu/settings_menu.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,7 +10,15 @@ func _ready():
 func _process(delta):
 	pass
 
+func _on_fullscreen_button_pressed():
+	if(DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
 func _on_back_button_pressed():
+	#TODO add backing to previous scene
 	MenuAudio.buttonPressedSound()
 #	get_tree().change_scene_to_file("res://KCards/scenes/menu/mode_menu.tscn")
 	SceneTransition.change_buttons(self,"res://KCards/scenes/menu/main_menu.tscn")
