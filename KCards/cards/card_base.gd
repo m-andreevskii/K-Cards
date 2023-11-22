3b5608cfe2eb24bdcf231a9c622a27c57440c924
Lekvf <92939783+Lekvf@users.noreply.github.com>
2023-11-22 18:36:42 +0700

    added cards

--- KCards/cards/card_base.gd
+++ KCards/cards/card_base.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends Node2D
 
 @onready 
 var CardsDatabase = preload("res://KCards/cards/CardsDatabase.gd")
@@ -9,32 +9,53 @@ var Cardname = $Bars/Name/CenterContainer/nameBackground/Name
 @onready 
 var CardImage
 
+var index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	display_card()
+	pass
 	
-	Cardname = "Scarecrew"
-	var CardInfo = CardsDatabase.DATA[myCardsDatabase.get(Cardname)]
-	var CardSize = size
-	var Attack = str(CardInfo[2])
-	var Health = str(CardInfo[3])
-	var Cost = str(CardInfo[4]) 
-	var Description = str(CardInfo[6])
-	CardImage = str("res://KCards/images/card/card_images/", CardInfo[7])
-	$Background.scale *= CardSize/$Background.texture.get_size()
-	$Bars/Name/CenterContainer/nameBackground.scale *= CardSize/$Background.texture.get_size()
-	$Bars/Image.texture = load(CardImage)
-	if ($Bars/Image.texture != null):
-		$Bars/Image.scale *= CardSize/($Bars/Image.texture.get_size())
+func display_card():
+	var CardInfo = myCardsDatabase.DATA[index]
+	
+	var CardSize = $Button.size
+	var type = CardInfo[1]
+	if type == "Ability":
+		$Button/Bars/BottomBar/Attack.visible = false
+		$Button/Bars/BottomBar/Health.visible = false
+	else: 
+		$Button/Bars/BottomBar/Attack.visible = true
+		$Button/Bars/BottomBar/Health.visible = true
+	CardImage = str("res://KCards/images/card/card_images/", CardInfo[5])
+	$Button/Background.scale = CardSize/$Button/Background.texture.get_size()
+	#$Bars/Name/CenterContainer/nameBackground.scale *= CardSize/$Background.texture.get_size()
+	$Button/Bars/Image.texture = load(CardImage)
+	if ($Button/Bars/Image.texture != null):
+		$Button/Bars/Image.scale = CardSize/($Button/Bars/Image.texture.get_size())
 
-	$Bars/Name/CenterContainer/nameBackground/Name.text = CardInfo[5]
-	$Bars/TopBar/Cost/CenterContainer/costBackground/Cost.text = Cost
-	$Bars/Description/CenterContainer/descrBackground/Description.text = Description
-	$Bars/BottomBar/Attack/CenterContainer/attackBackground/Attack.text = Attack
-	$Bars/BottomBar/Health/CenterContainer/healthBackground/Health.text = Health
+	if type != "Ability":
+		var Attack = str(CardInfo[6])
+		var Health = str(CardInfo[7])
+		$Button/Bars/BottomBar/Attack/CenterContainer/attackBackground/Attack.text = Attack
+		$Button/Bars/BottomBar/Health/CenterContainer/healthBackground/Health.text = Health
 
+	var Cost = str(CardInfo[2]) 
+	var Description = str(CardInfo[4])
+	$Button/Bars/Name/CenterContainer/nameBackground/Name.text = CardInfo[3]
+	$Button/Bars/TopBar/Cost/CenterContainer/costBackground/Cost.text = Cost
+	$Button/Bars/Description/CenterContainer/descrBackground/Description.text = Description
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 """@warning_ignore("unused_parameter")
 func _process(delta):
 	pass
 """
+
+
+func _on_button_pressed():
+	index+= 1
+	if index >= 9:
+		get_tree().quit()
+	display_card()
+	
