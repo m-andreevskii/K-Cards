782f679c5dd24db6e04e28bbe7f61fba7314d0c3
mu2so4 <b4.orangutang@gmail.com>
2023-12-06 12:43:39 +0700

    Remove unneccessary files

--- KCards/scenes/menu/battlefield/battlefield.gd
+++ KCards/scenes/menu/battlefield/battlefield.gd
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_move_button_pressed():
+	MenuAudio.buttonPressedSound()
+	pass
